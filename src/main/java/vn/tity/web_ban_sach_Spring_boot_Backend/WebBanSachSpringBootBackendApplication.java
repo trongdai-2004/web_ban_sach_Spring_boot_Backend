03bc9f2f65455b23f5769f085141cb121e93f9eb
Trong Dai <caotrongdai2004cd@gmail.com>
2026-09-16 11:15:30 +0700

    khởi tạo dự án

---
+++ src/main/java/vn/tity/web_ban_sach_Spring_boot_Backend/WebBanSachSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.tity.web_ban_sach_Spring_boot_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachSpringBootBackendApplication {
+
+	public static voifd main(String[] args) {
+		SpringApplication.run(WebBanSachSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/tity/web_ban_sach_Spring_boot_Backend/WebBanSachSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tity.web_ban_sach_Spring_boot_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanSachSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
